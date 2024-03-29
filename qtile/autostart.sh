0e2450de564ad04080aa30c68e9815a715579071
StealYourCode <90696735+StealYourCode@users.noreply.github.com>
2024-03-29 22:28:13 +0100

    Create autostart.sh

---
+++ qtile/autostart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#   ___ _____ ___ _     _____   ____  _             _    
+#  / _ \_   _|_ _| |   | ____| / ___|| |_ __ _ _ __| |_  
+# | | | || |  | || |   |  _|   \___ \| __/ _` | '__| __| 
+# | |_| || |  | || |___| |___   ___) | || (_| | |  | |_  
+#  \__\_\|_| |___|_____|_____| |____/ \__\__,_|_|   \__| 
+#                                                        
+#  
+# ----------------------------------------------------- 
+# Fork of https://gitlab.com/stephan-raabe/dotfiles/-/blob/main/qtile/autostart.sh?ref_type=heads
+
+# My screen resolution
+# xrandr --rate 60
+
+# Keyboard layout
+setxkbmap be
+
+# Load picom
+picom &
